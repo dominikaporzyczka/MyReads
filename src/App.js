27e772610f1b31a017bd1da6956a940d17fe264b
Dominika Porzyczka <dominikaporzyczka@gmail.com>
2018-07-05 19:52:44 +0200

    feat: Create BooksApp component

--- src/App.js
+++ src/App.js
@@ -1,1 +1,11 @@
 import React, { Component } from 'react';
+
+class BooksApp extends Component {
+    render() {
+        return (
+            <h1>HELLO! Here will be your bookshelves :)</h1>
+        )  
+    }
+}
+
+export default BooksApp;
