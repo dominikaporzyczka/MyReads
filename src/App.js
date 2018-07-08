735238cf8e343e3635bb76891078f18e59474fc3
Dominika Porzyczka <dominikaporzyczka@gmail.com>
2018-07-08 11:52:47 +0200

    feat: Add route

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom'
+import { Link, Route } from 'react-router-dom'
 import './App.css'
+import SearchBooks from './SearchBooks';
 
 class App extends Component {
     render() {
         return (
-            <div className="list-books">
-                <div className="list-books-title">
-                    <h1>MyReads</h1>
-                </div>
-                <div className="open-search">
-                    <Link
-                        to="/search"
-                    >Add a book</Link>
-                </div>
+            <div className="app">
+                <Route exact path="/" render={() => (
+                    <div className="list-books">
+                        <div className="list-books-title">
+                            <h1>MyReads</h1>
+                        </div>
+                        <div className="open-search">
+                            <Link
+                                to="/search"
+                            >Add a book</Link>
+                        </div>
+                    </div>
+                )} />
+                <Route path="/search" render={() => (
+                    <SearchBooks/>
+                )}/>
             </div>
         )
     }
--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 import './App.css'
 
 class SearchBooks extends Component {
@@ -6,7 +7,7 @@ class SearchBooks extends Component {
         return (
             <div className="search-books">
                 <div className="search-books-bar">
-                    <a className="close-search">Close</a>
+                    <Link className="close-search" to="/">Close</Link>
                     <input type="text" placeholder="Search by title or author"/>
                 </div>
             </div>
