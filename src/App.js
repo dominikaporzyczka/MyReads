428f66aa0df2efdc749c2d8c2d05e3852857e577
Dominika Porzyczka <dominikaporzyczka@gmail.com>
2018-07-08 14:22:55 +0200

    refactor: Create new component - ListBooks

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
-import { Link, Route } from 'react-router-dom'
-import SearchBooks from './SearchBooks';
-import Bookshelf from './Bookshelf'
+import React, { Component } from 'react'
+import { Route } from 'react-router-dom'
+import ListBooks from './ListBooks'
+import SearchBooks from './SearchBooks'
 import './App.css'
 
 class App extends Component {
@@ -9,19 +9,7 @@ class App extends Component {
         return (
             <div className="app">
                 <Route exact path="/" render={() => (
-                    <div className="list-books">
-                        <div className="list-books-title">
-                            <h1>MyReads</h1>
-                        </div>
-                        <div className="list-books-content">
-                            <Bookshelf/>
-                        </div>
-                        <div className="open-search">
-                            <Link
-                                to="/search"
-                            >Add a book</Link>
-                        </div>
-                    </div>
+                    <ListBooks/>
                 )} />
                 <Route path="/search" render={() => (
                     <SearchBooks/>
